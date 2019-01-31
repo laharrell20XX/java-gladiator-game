866f9cca4e8478761cdd2c323a267bba54c0bd2c
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-01-31 15:57:41 -0600

    can run demo game between two gladiators: attacker, defender (no input)

--- src/GladiatorGameRunner.java
+++ src/GladiatorGameRunner.java
@@ -1,5 +1,22 @@
-public class GladiatorGameRunner {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class GladiatorGameRunner {
+
+    public static void main(String[] args) {
+        Gladiator bob = new Gladiator("attacker", 100, 0, 5, 15);
+        Gladiator bill = new Gladiator("defender", 100, 0, 5, 15);
+        GladiatorGame game = new GladiatorGame(bob, bill);
+        while (!game.isGameOver()) {
+            System.out.println(bill);
+            System.out.println(bob);
+            run(game);
+        }
+        System.out.println(game.getDefender().getName() + " wins!");
     }
+    private static void run(GladiatorGame game) {
+        TurnResult result = game.attack();
+        System.out.println(result);
+        game.swapTurns();
+    }
+
 }
