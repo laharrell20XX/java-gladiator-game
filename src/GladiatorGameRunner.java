5fcdaa99c039ddb8981f9c1c56d3c8714f288627
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-01 09:36:26 -0600

    added input for the gladiators, with validation; cleaned up run()
    Input
      -  getName() asks a player for their name which is used in the
      Gladiator constructor
      - getDecision() asks a player to make a choice on what they want to
      do for their turn; attack, heal, or pass; isValidDecision() is run on
      what they choose to see if it's allowed
    
    Validation
      - processDecision() is a helper that builds the result string based
      on the decision of the gladiator; involves making a TurnResult object
      and calling the appropriate methods on it
    
    Cleanup
      - moved logic into helper method processDecision() and replaced the
      automatic attack turn with input

--- src/GladiatorGameRunner.java
+++ src/GladiatorGameRunner.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class GladiatorGameRunner {
+    private static Scanner stdin = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Gladiator bob = new Gladiator("attacker", 100, 0, 5, 15);
-        Gladiator bill = new Gladiator("defender", 100, 0, 5, 15);
+        Gladiator bob = new Gladiator(getName(), 100, 0, 5, 15);
+        Gladiator bill = new Gladiator(getName(), 100, 0, 5, 15);
         GladiatorGame game = new GladiatorGame(bob, bill);
         while (!game.isGameOver()) {
             System.out.println(bill);
@@ -14,9 +15,49 @@ public class GladiatorGameRunner {
         System.out.println(game.getDefender().getName() + " wins!");
     }
     private static void run(GladiatorGame game) {
-        TurnResult result = game.attack();
-        System.out.println(result);
+        String resultStr = processDecision(game, getDecision(game.getAttacker().getName()));
+        System.out.println(resultStr);
         game.swapTurns();
     }
 
+    private static String processDecision(GladiatorGame game, int decision) {
+        TurnResult result = new TurnResult(game.getAttacker(), game.getDefender());
+        String resultStr = "";
+        if (decision == 1) {
+            game.attack();
+            resultStr=result.attackTurnResult();
+        }
+        if (decision == 2) {
+            if (game.heal()) {resultStr = result.healTurnResult();}
+            else {resultStr = result.failHealTurnResult();}
+        }
+        if (decision == 3) {
+            resultStr = result.passTurnResult();
+        }
+        return resultStr;
+    }
+
+    private static String getName() {
+        System.out.println("Player, what is your gladiator name?\n>>> ");
+        String playerName = stdin.nextLine();
+        System.out.println(playerName + " has entered the arena\n");
+        return playerName;
+    }
+    private static int getDecision(String attackerName) {
+        boolean isValidDecision = false;
+        int decision = 0;
+        while (!isValidDecision) {
+            System.out.println(attackerName + ", what do you want to do?");
+            System.out.println("1: attack");
+            System.out.println("2: heal");
+            System.out.println("3: pass\n>>>");
+            decision = stdin.nextInt();
+            if (isValidDecision(decision)) {isValidDecision = true;}
+        }
+        return decision;
+    }
+
+    private static boolean isValidDecision(int decision) {
+        return decision >= 1 && decision <= 3;
+    }
 }
